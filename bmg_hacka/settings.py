8c81093fa064eb61dde17b5349ac9ed15756480e
Matheus Mendes <ronaldo_rogerio@yahoo.com.br>
2020-05-30 17:34:36 -0300

    debug true

--- bmg_hacka/settings.py
+++ bmg_hacka/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7(!l8xur3zt)&(9r+!36v@g$z6(9$*(ei()#!(n!0909z(lqp%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://bmg-hacka.herokuapp.com/',]
 
